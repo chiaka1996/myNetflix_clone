c1f3c5fd863fe84317a252550645d807a8969fc5
newton <chiakajunior@yahoo.com>
2021-08-09 15:33:27 +0100

    working on facebook auth

--- js/fb.js
+++ js/fb.js
@@ -1,17 +1,28 @@
-let loginWithFacebook = _ => _
-
-const fbSDKLoaded = () => {
-    
-FB.getLoginStatus(function(response) {
-    // console.log(statusChangeCallback(response));
-    console.log(response);
-    if(response.status == "not_authorized"){
-        loginWithFacebook = _ => {
-            FB.login(response => {
-                console.log(response);
-            })
-        }
-    }
-});
-
+const loginWithFacebookSpan  = document.querySelector('.facebookLogin');
+ 
+const loginWithFacebook = () => {
+    FB.login( response => {
+       console.log(response) 
+    }, {scope: 'public_profile, email'})
+    return false
 }
+
+loginWithFacebookSpan.addEventListener('click', loginWithFacebook, false);
+
+// let loginWithFacebook = _ => _
+
+// const fbSDKLoaded = () => {
+    
+// FB.getLoginStatus(function(response) {
+//     // console.log(statusChangeCallback(response));
+//     console.log(response);
+//     if(response.status == "not_authorized"){
+//         loginWithFacebook = _ => {
+//             FB.login(response => {
+//                 console.log(response);
+//             })
+//         }
+//     }
+// });
+
+// }
