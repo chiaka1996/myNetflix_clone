19fefd268221e7944eaeaeff3fc647a0f48fd77f
newton <chiakajunior@yahoo.com>
2021-08-09 14:46:07 +0100

    still working

--- js/fb.js
+++ js/fb.js
@@ -1,7 +1,8 @@
 const fbSDKLoaded = () => {
     
 FB.getLoginStatus(function(response) {
-    console.log(statusChangeCallback(response));
+    // console.log(statusChangeCallback(response));
+    console.log(response);
 });
 
 }
