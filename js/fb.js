c90fb3ed8b100c0836da7894cfbd7fc40c0b1dae
newton <chiakajunior@yahoo.com>
2021-08-09 15:14:20 +0100

    working

--- js/fb.js
+++ js/fb.js
@@ -1,8 +1,17 @@
+let loginWithFacebook = _ => _
+
 const fbSDKLoaded = () => {
     
 FB.getLoginStatus(function(response) {
     // console.log(statusChangeCallback(response));
     console.log(response);
+    if(response.status == "not_authorized"){
+        loginWithFacebook = _ => {
+            FB.login(response => {
+                console.log(response);
+            })
+        }
+    }
 });
 
 }
